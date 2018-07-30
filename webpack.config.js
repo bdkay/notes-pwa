19ea084e85d63c430fb6de18dbea75172c3452bc
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-30 16:49:16 -0500

    Maybe some webpack for later

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: ['./index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }
+};
