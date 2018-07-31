ff7a7ec65e2560700abec3dadb8a4eafefa17c35
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-07-31 15:15:58 -0500

    Promises promises

--- promise/promise.js
+++ promise/promise.js
@@ -1,15 +1,18 @@
 function add(number, callback){
   return new Promise(function(resolve, reject) {
-    if (number > 4){
+    if (number > 0) {
       reject('You cannot have more than 4');
-      setTimeout(function (){
     }
+    setTimeout(function (){
       resolve(number + 1);
-    }, 1000);
+      }, 1000);
   });
 }
 
 add(1)
   .then(function(result){
     console.log(result);
+  })
+  .catch(function(error){
+    console.log(error);
   });
